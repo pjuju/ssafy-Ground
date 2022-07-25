ab49c237d4d22393b0aed69414caafb20970bf2d
한유빈 <hyb120934@gmail.com>
2022-07-25 16:42:16 +0900

    style : scss 파일 추가

--- frontend/src/components/RectButton.js
+++ frontend/src/components/RectButton.js
@@ -4,6 +4,7 @@ import { Button } from '@mui/material';
 const CustomButton = styled(Button)({
     color: '#ffffff',
     backgroundColor: '#54BAB9',
+    size: 'middle',
     '&:hover': {
         backgroundColor: '#18978F',
       },
