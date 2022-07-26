00984450a85f337188e0ae6e73fa87434f821769
nullyng <nullyng@gmail.com>
2022-07-26 20:24:16 +0900

    chore : 루트 렌더링 컴포넌트 변경
    LoginPage에서 App으로 첫 화면에서 렌더링 될 컴포넌트 변경

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
-import LoginPage from './pages/LoginPage';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <LoginPage/>
+    <App/>
   </React.StrictMode>
 );
