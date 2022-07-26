d0683916a3b74495f86c310edb8d345aeecbdfed
Mintfrog_1209 <hyb120934@gmail.com>
2022-07-27 02:00:42 +0900

    [S07P12D103-122] feat : 아이디 및 비밀번호 찾기 결과 페이지 생성

--- frontend/src/components/RectButton.js
+++ frontend/src/components/RectButton.js
@@ -4,11 +4,9 @@ import { Button } from '@mui/material';
 const CustomButton = styled(Button)({
     color: '#ffffff',
     backgroundColor: '#54BAB9',
-    size: 'middle', 
     '&:hover': {
         backgroundColor: '#18978F',
       },
-    width: '80%',
 });
 
 export function RectButton(props) {
