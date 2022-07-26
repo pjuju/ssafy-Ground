311912c74fb7952a1933b3a743ffa6f6d1e3e00a
nullyng <nullyng@gmail.com>
2022-07-26 13:33:30 +0900

    [S07P12D103-98] feat : 소셜 로그인 리다이렉트 핸들러 생성
    - 구글, 카카오 로그인 후 redirect 되는 화면에서 사용할 컴포넌트 생성
    - App.js에서 각각의 redirect 경로에서 보여줄 컴포넌트 지정
    - 아직 백엔드와 통신이 불가능하여 통신 부분은 주석 처리함
    
    참조 url : https://velog.io/@nuri00/Google-OAuth-%EB%A1%9C%EA%B7%B8%EC%9D%B8-%EA%B5%AC%ED%98%84, https://data-jj.tistory.com/53
    관련 이슈 : S07P12D103-75, S07P12D103-99

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import LoginPage from './pages/LoginPage';
+import KakaoRedirectHandler from 'components/Login/OAuth/KakaoRedirectHandler';
+import GoogleRedirectHandler from 'components/Login/OAuth/GoogleRedirectHandler';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginPage />} />
+        <Route path="/oauth/callback/google" element={<GoogleRedirectHandler />} />
+        <Route path="/oauth/callback/kakao" element={<KakaoRedirectHandler />} />
       </Routes>
     </BrowserRouter>
   );
