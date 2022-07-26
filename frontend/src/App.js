1bd13990e0b6b27fc277d10646ecd9cee8fc20c4
nullyng <nullyng@gmail.com>
2022-07-26 20:25:38 +0900

    [S07P12D103-100] feat : 프로필 선택 페이지 UI 생성
    - 프로필 선택 페이지 UI 생성
    - 프로필 선택 페이지 스타일시트 작성
    
    관련 이슈 : S07P12D103-86

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,17 +4,19 @@ import KakaoRedirectHandler from 'components/Login/OAuth/KakaoRedirectHandler';
 import GoogleRedirectHandler from 'components/Login/OAuth/GoogleRedirectHandler';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import WelcomePage from 'pages/WelcomePage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route exact path="/" element={<LoginPage />} />
         <Route path="/oauth/callback/google" element={<GoogleRedirectHandler />} />
         <Route path="/oauth/callback/kakao" element={<KakaoRedirectHandler />} />
+        <Route path="/welcome" element={<WelcomePage />} />
       </Routes>
     </BrowserRouter>
   );
 }
 
-// export default App;
+export default App;
---
+++ frontend/src/pages/WelcomePage.js
@@ -0,0 +1,64 @@
+import logo from "assets/images/text_logo.png";
+import userImageInput from "assets/images/userImageInput.png";
+import "styles/Welcome/WelcomePage.scss";
+import theme from "components/common/theme.js";
+
+import { useState } from "react";
+import { Button, Grid } from "@mui/material";
+import { ThemeProvider } from "@emotion/react";
+
+function WelcomePage() {
+  const [userImg, setUserImg] = useState("");
+  const [userDesc, setUserDesc] = useState("");
+  const [userInterest, setUserInterest] = useState([]);
+  const [first, setfirst] = useState(second)
+
+  const [isSetImg, setIsSetImg] = useState(false);
+  const [isSetDesc, setIsSetDesc] = useState(false);
+  const [isSetInterest, setIsSetInterest] = useState(false);
+
+  const handleImgButton = () => {
+    if(userImg === "") {
+      console.log("건너뛰기");
+    } else {
+      console.log("다음");
+    }
+  }
+
+  return (
+    <Grid
+      className="user-settings"
+      container
+      direction="column"
+      justifyContent="center"
+      alignItems="center">
+      <img className="logo" src={logo} alt="text_logo" width="300px" />
+      <Grid item>
+      { isSetImg === false && 
+        <Grid className="user-settings__user-img" container direction="column">
+          <Grid item>
+            <h3 className="user-settings__user-img--title">
+              자신을 나타낼 사진을 선택해 주세요!
+            </h3>
+          </Grid>
+          <Grid item>
+            <img className="user-settings__user-img--input" src={userImageInput} alt="user-img-input" />
+          </Grid>
+            <ThemeProvider theme={theme} >
+              <Button
+                className="user-settings__user-img--input"
+                variant={userImg === "" ? "outlined" : "contained"}
+                onClick={handleImgButton}>
+                {userImg === "" ? "건너뛰기" : "다음"}
+              </Button>
+            </ThemeProvider>
+          <Grid className="user-settings__uesr-img--button">
+          </Grid>
+        </Grid>
+      }
+    </Grid>
+  </Grid>
+  );
+}
+
+export default WelcomePage;
