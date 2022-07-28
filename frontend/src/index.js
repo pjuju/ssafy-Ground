6c25bb88053125a5bbc0eb2a4c915af8528bc6e2
Inhu Jo <inhudev@gmail.com>
2022-07-28 15:45:10 +0900

    fix : index.js, App.js 수정
    - index.js: 루트에서 App 렌더
    - App.js: export default 주석 해제

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-// import App from './App';
-import LoginPage from "./pages/LoginPage";
-// import ComponentTest from "pages/ComponentTest";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+// import LoginPage from './pages/LoginPage';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <LoginPage/>
+    <App />
   </React.StrictMode>
 );
