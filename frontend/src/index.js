f644c6555b06b7509dc5411ad351d7f4455e3110
Inhu Jo <inhudev@gmail.com>
2022-07-28 13:08:20 +0900

    merge from front to front-feature/user/register

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 // import LoginPage from './pages/LoginPage';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
