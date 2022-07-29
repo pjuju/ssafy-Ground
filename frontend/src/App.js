d84c8bd2788a2fbf9e368f7d853daa8f8b215b9d
nullyng <nullyng@gmail.com>
2022-07-29 15:47:57 +0900

    chore : App.js Prettier 정렬

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,24 @@
-import './App.css';
-import LoginPage from './pages/LoginPage';
-import KakaoRedirectHandler from 'components/Login/OAuth/KakaoRedirectHandler';
-import GoogleRedirectHandler from 'components/Login/OAuth/GoogleRedirectHandler';
+import "./App.css";
+import LoginPage from "./pages/LoginPage";
+import KakaoRedirectHandler from "components/Login/OAuth/KakaoRedirectHandler";
+import GoogleRedirectHandler from "components/Login/OAuth/GoogleRedirectHandler";
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import WelcomePage from 'pages/WelcomePage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import WelcomePage from "pages/WelcomePage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={<LoginPage />} />
-        <Route path="/oauth/callback/google" element={<GoogleRedirectHandler />} />
-        <Route path="/oauth/callback/kakao" element={<KakaoRedirectHandler />} />
+        <Route path="/" element={<LoginPage />} />
+        <Route
+          path="/oauth/callback/google"
+          element={<GoogleRedirectHandler />}
+        />
+        <Route
+          path="/oauth/callback/kakao"
+          element={<KakaoRedirectHandler />}
+        />
         <Route path="/welcome" element={<WelcomePage />} />
       </Routes>
     </BrowserRouter>
