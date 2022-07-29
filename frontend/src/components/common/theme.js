3fdd5882921ef047650f90e9d5ae5ec41b187d9e
nullyng <nullyng@gmail.com>
2022-07-29 21:01:16 +0900

    chore : theme.js conflict 수정

--- frontend/src/components/common/theme.js
+++ frontend/src/components/common/theme.js
@@ -1,26 +1,18 @@
-import { createTheme } from '@mui/material/styles';
+import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
   status: {
-    danger: '#e53e3e',
+    danger: "#e53e3e",
   },
   palette: {
     primary: {
-      main: '#54BAB9',
-      contrastText: '#fff',
+      main: "#54BAB9",
+      contrastText: "#fff",
     },
     secondary: {
-      main: '#18978F'
-    }
+      main: "#18978F",
+    },
   },
 });
 
-<<<<<<< HEAD
 export default theme;
-=======
-<<<<<<< HEAD
-export default theme;
-=======
-export default theme;
->>>>>>> front-feature/user/find-id
->>>>>>> 7074d55b09278a6ad95da38546c8bd80ff8c40f4
