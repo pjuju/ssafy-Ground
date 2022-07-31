7ede91d96a18f162896df7efdcd18a23c4bfedcc
조인후 <lovelinus57@naver.com>
2022-07-31 17:30:30 +0900

    [S07P12D103-12] feat : 회원가입 폼 css 적용

--- frontend/src/components/Register/OtherInfo.js
+++ frontend/src/components/Register/OtherInfo.js
@@ -1,6 +1,6 @@
 import Grid from "@mui/material/Grid";
-import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
+// import TextField from "@mui/material/TextField";
+// import Button from "@mui/material/Button";
 import FormControl from "@mui/material/FormControl";
 import Divider from "@mui/material/Divider";
 import FormControlLabel from "@mui/material/FormControlLabel";
@@ -9,8 +9,8 @@ import MenuItem from "@mui/material/MenuItem";
 import Radio from "@mui/material/Radio";
 import RadioGroup from "@mui/material/RadioGroup";
 import Select from "@mui/material/Select";
-
-import "styles/Register/RegisterPage.scss";
+import GrButton from "components/common/GrButton";
+import GrTextField from "components/common/GrTextField";
 
 import { useState } from "react";
 
@@ -30,31 +30,79 @@ const ageList = ages.map((item, index) => (
   </MenuItem>
 ));
 
-function OtherInfo() {
+function OtherInfo({ changeOtherInfo, sendRequest }) {
+  const [nickName, setNickName] = useState("");
   const [age, setAge] = useState(ages[0].value);
+  const [gender, setGender] = useState("male");
+
+  // 회원가입 버튼 핸들러
+  const onClickRegister = () => {
+    const newOtherInfo = {
+      nickname: nickName,
+      age: age,
+      gender: gender,
+    };
+    changeOtherInfo(newOtherInfo);
+
+    sendRequest();
+  };
 
   return (
-    <Grid className="register-form__bottom" item>
-      <Grid className="id-field" container justifyContent="center">
-        <TextField size="small" label="닉네임" />
-        <Button variant="contained">중복 확인</Button>
+    <>
+      <Grid
+        className="register-form__inner-wrapper"
+        container
+        justifyContent="space-between"
+      >
+        <GrTextField
+          className="register-form__field"
+          size="small"
+          label="닉네임"
+          value={nickName}
+          onChange={(e) => setNickName(e.target.value)}
+        />
+        <GrButton className="register-form__innerBtn" variant="contained">
+          중복확인
+        </GrButton>
       </Grid>
-      <Grid className="select-field" container justifyContent="space-between">
+      <Grid
+        className="register-form__select-field"
+        container
+        justifyContent="space-between"
+      >
         <FormControl sx={{ minWidth: 180 }} size="small">
           <InputLabel id="age-label">연령대</InputLabel>
-          <Select labelId="age-label" label="연령대" value={age}>
+          <Select
+            labelId="age-label"
+            label="연령대"
+            value={age}
+            onChange={(e) => {
+              setAge(e.target.value);
+            }}
+          >
             {ageList}
           </Select>
         </FormControl>
         <Divider orientation="vertical" flexItem />
         <FormControl>
-          <RadioGroup row>
-            <FormControlLabel value="female" control={<Radio />} label="남" />
-            <FormControlLabel value="male" control={<Radio />} label="여" />
+          <RadioGroup
+            row
+            value={gender}
+            onChange={(e) => setGender(e.target.value)}
+          >
+            <FormControlLabel value="male" control={<Radio />} label="남" />
+            <FormControlLabel value="female" control={<Radio />} label="여" />
           </RadioGroup>
         </FormControl>
       </Grid>
-    </Grid>
+      <GrButton
+        className="register-form__button"
+        variant="contained"
+        onClick={onClickRegister}
+      >
+        회원가입
+      </GrButton>
+    </>
   );
 }
 
