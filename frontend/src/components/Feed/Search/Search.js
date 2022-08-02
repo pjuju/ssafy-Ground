98c9bd1fe425bf6c502cb5ff7f3398c7a248d1a4
Inhu Jo <inhudev@gmail.com>
2022-08-03 00:09:27 +0900

    [S07P12D103-136] feat : 검색 기준 선택 드롭다운 추가

--- frontend/src/components/Feed/Search/Search.js
+++ frontend/src/components/Feed/Search/Search.js
@@ -1,5 +1,23 @@
+const { Grid, Select, FormControl, InputLabel, MenuItem } = require("@mui/material");
+
 function Search() {
-  return <>search</>;
+  return (
+    <Grid className="search-inner" container direction="column">
+      <Grid className="search-inner__top" container direction="column">
+        <Grid className="top__search-bar" container>
+          <FormControl>
+            <InputLabel id="search-standard">기준 선택</InputLabel>
+            <Select labelId="search-standard" label="기준 선택">
+              <MenuItem value="board">게시글</MenuItem>
+              <MenuItem value="user">유저</MenuItem>
+            </Select>
+          </FormControl>
+          <Grid item>검색바</Grid>
+          <Grid item>버튼</Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 }
 
 export default Search;
