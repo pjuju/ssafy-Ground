9e1b7c86b3dcf197a3c592a84d0edaeb074d0c61
Inhu Jo <inhudev@gmail.com>
2022-08-03 00:24:02 +0900

    [S07P12D103-136] feat : 검색 상단 layout 변경
    - Grid 속성 item으로 변경

--- frontend/src/components/Feed/Search/Search.js
+++ frontend/src/components/Feed/Search/Search.js
@@ -1,21 +1,21 @@
-const { Grid, Select, FormControl, InputLabel, MenuItem } = require("@mui/material");
+const {
+  Grid,
+  Select,
+  FormControl,
+  InputLabel,
+  MenuItem,
+} = require("@mui/material");
 
 function Search() {
   return (
-    <Grid className="search-inner" container direction="column">
-      <Grid className="search-inner__top" container direction="column">
-        <Grid className="top__search-bar" container>
-          <FormControl>
-            <InputLabel id="search-standard">기준 선택</InputLabel>
-            <Select labelId="search-standard" label="기준 선택">
-              <MenuItem value="board">게시글</MenuItem>
-              <MenuItem value="user">유저</MenuItem>
-            </Select>
-          </FormControl>
-          <Grid item>검색바</Grid>
-          <Grid item>버튼</Grid>
-        </Grid>
-      </Grid>
+    <Grid item>
+      <FormControl>
+        <InputLabel id="search-standard">기준 선택</InputLabel>
+        <Select labelId="search-standard" label="기준 선택">
+          <MenuItem value="board">게시글</MenuItem>
+          <MenuItem value="user">유저</MenuItem>
+        </Select>
+      </FormControl>
     </Grid>
   );
 }
