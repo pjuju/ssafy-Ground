23dfb8d3feee0c10b25d7a284718eea3241f0460
nullyng <nullyng@gmail.com>
2022-08-03 15:06:55 +0900

    chore : 프록시 및 api 설정
    - 프록시 base url을 localhost:8080으로 설정
    - index.js 오타 수정

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -4,7 +4,7 @@ function apiInstance() {
   const instance = axios.create({
     baseURL: "http://localhost:3000/api",
     headers: {
-      "Content-tupe": "application/json",
+      "Content-type": "application/json",
     },
   });
   return instance;
