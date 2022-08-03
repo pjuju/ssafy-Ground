197961bc8b670b0470cbcd24ad4fcfc056b5d215
InhuJo <inhudev@gmail.com>
2022-08-03 10:45:36 +0900

    chore : material-icon 삭제

--- frontend/src/components/Feed/Search/Search.js
+++ frontend/src/components/Feed/Search/Search.js
@@ -1,21 +1,18 @@
-const {
-  Grid,
-  Select,
-  FormControl,
-  InputLabel,
-  MenuItem,
-} = require("@mui/material");
+import { Grid, Select, FormControl, InputLabel, MenuItem } from "@mui/material";
+import "styles/Search/Search.scss";
 
 function Search() {
   return (
-    <Grid item>
-      <FormControl>
-        <InputLabel id="search-standard">기준 선택</InputLabel>
-        <Select labelId="search-standard" label="기준 선택">
-          <MenuItem value="board">게시글</MenuItem>
-          <MenuItem value="user">유저</MenuItem>
-        </Select>
-      </FormControl>
+    <Grid className="search-inner" item>
+      <Grid container>
+        <FormControl>
+          <InputLabel id="search-standard">기준 선택</InputLabel>
+          <Select labelId="search-standard" label="기준 선택">
+            <MenuItem value="board">게시글</MenuItem>
+            <MenuItem value="user">유저</MenuItem>
+          </Select>
+        </FormControl>
+      </Grid>
     </Grid>
   );
 }
