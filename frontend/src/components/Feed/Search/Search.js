77e474cab6a34d27ace9158262842ce9d683d880
InhuJo <inhudev@gmail.com>
2022-08-03 15:39:01 +0900

    [S07P12D103-139] feat : 검색 페이지에 필터 버튼 및 모달 추가

--- frontend/src/components/Feed/Search/Search.js
+++ frontend/src/components/Feed/Search/Search.js
@@ -1,11 +1,58 @@
-import { Grid, Select, FormControl, InputLabel, MenuItem } from "@mui/material";
+import {
+  Grid,
+  Select,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  IconButton,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
+  Typography,
+  Modal,
+  Box,
+} from "@mui/material";
 import GrButton from "components/common/GrButton";
 import GrTextField from "components/common/GrTextField";
 import { useForm, Controller } from "react-hook-form";
 import "styles/Search/Search.scss";
 
+import FilterAltIcon from "@mui/icons-material/FilterAlt";
+import { useState } from "react";
+import FilterModal from "./FilterModal";
+
+const date = [
+  { value: "whole", label: "전체" },
+  { value: "today", label: "오늘" },
+  { value: "week", label: "1주일" },
+  { value: "month", label: "1개월" },
+  { value: "year", label: "1년" },
+  { value: "custom", label: "직접 입력" },
+];
+
+const dateRadio = date.map((item, index) => (
+  <FormControlLabel
+    className="top__date-select"
+    key={index}
+    value={item.value}
+    label={<Typography sx={{ fontSize: "0.9rem" }}>{item.label}</Typography>}
+    control={<Radio size="small" />}
+  />
+));
+
 function Search() {
-  const { control } = useForm({ defaultValues: { standard: "", word: "" } });
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const { control, handleSubmit } = useForm({
+    defaultValues: { standard: "", word: "", date: "" },
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <Grid className="search-inner" item>
       <form>
@@ -51,11 +98,40 @@ function Search() {
               />
             </Grid>
             <Grid xs={1} item>
-              <GrButton className="top__search-button" variant="contained">
+              <GrButton
+                className="top__search-button"
+                variant="contained"
+                onClick={handleSubmit(onSubmit)}
+              >
                 검색
               </GrButton>
             </Grid>
+            <Grid xs={1} item>
+              <Grid container justifyContent="center">
+                <IconButton onClick={handleOpen}>
+                  <FilterAltIcon />
+                </IconButton>
+                <FilterModal open={open} handleClose={handleClose} />
+              </Grid>
+            </Grid>
           </Grid>
+          {
+            !(
+              <Grid container>
+                <FormControl>
+                  <Controller
+                    name="date"
+                    control={control}
+                    render={({ field }) => (
+                      <RadioGroup row {...field}>
+                        {dateRadio}
+                      </RadioGroup>
+                    )}
+                  />
+                </FormControl>
+              </Grid>
+            )
+          }
         </Grid>
       </form>
     </Grid>
