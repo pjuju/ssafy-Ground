b26c3223402342796784e83f1fbb2a08de8f1b6d
한유빈 <hyb120934@gmail.com>
2022-08-03 15:19:01 +0900

    [S07P12D103-15] refactor : tabPanel 수정

--- frontend/src/pages/Findid/FindIdPage.js
+++ frontend/src/pages/Findid/FindIdPage.js
@@ -2,15 +2,17 @@ import React from "react";
 import "styles/FindId/FindIdPage.scss";
 import { Grid }  from "@mui/material";
 import { Container } from "@mui/material";
-import { Tabs, Tab, Box } from "@mui/material";
+import { Tab, Box } from "@mui/material";
+import { TabPanel, TabList, TabContext } from "@mui/lab";
 import { ThemeProvider } from "@emotion/react";
 import theme from "components/common/theme.js";
 
 import logo from "assets/images/text_logo.png";
-import TabPanel from "components/FindId/TabPanel.js";
+import IdTab from "components/FindId/IdTab";
+import PasswordTab from "components/FindId/PasswordTab";
 
 function FindIdPage() {
-	const [value, setValue] = React.useState(0);
+	const [value, setValue] = React.useState('1');
 	
 
   const handleChange = (event, newValue) => {
@@ -18,32 +20,44 @@ function FindIdPage() {
   };
 
 	return (
-		<Container className="findid-form" maxwidth="xs" fixed>
-			<Grid
-				container
-				className="findid-form__wrapper"
-				direction="column"
-				justifyContent="center"
-				alignItems="center"
-			>
-				<Grid className="login-form__logo" item>
+    <Container className="findid-form" maxwidth="xs" fixed>
+      <Grid
+        container
+        className="findid-form__wrapper"
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Grid className="login-form__logo" item>
           <img className="logo" src={logo} alt="text_logo" width="300px" />
         </Grid>
-				<Grid item>
-					<ThemeProvider theme={theme}>
-						<Box sx={{ borderBottom: 1, borderColor: 'divider', margin: "1rem"}} >
-							<Tabs value={value} onChange={handleChange} aria-label="basic tabs example" variant="fullWidth">
-								<Tab label="아이디 찾기"  />
-								<Tab label="비밀번호 찾기"  />
-							</Tabs>
-						</Box>
-					</ThemeProvider>
-					<TabPanel value={value} index={0}/>
-					<TabPanel value={value} index={1}/>
-				</Grid>
-			</Grid>
-		</Container>
-	);
+        <Grid item>
+          <TabContext value={value}>
+            <ThemeProvider theme={theme}>
+              <Box
+                sx={{ borderBottom: 1, borderColor: "divider", margin: "1rem" }}
+              >
+                <TabList
+                  onChange={handleChange}
+                  aria-label="basic tabs example"
+                  variant="fullWidth"
+                >
+                  <Tab label="아이디 찾기" value="1"/>
+                  <Tab label="비밀번호 찾기" value="2" />
+                </TabList>
+              </Box>
+            </ThemeProvider>
+            <TabPanel value="1">
+              <IdTab />
+            </TabPanel>
+            <TabPanel value="2">
+              <PasswordTab />
+            </TabPanel>
+          </TabContext>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
 
 export default FindIdPage;
