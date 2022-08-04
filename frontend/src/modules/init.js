cce63cfb8ae15ec32680ea995fe596e79500f2db
nullyng <nullyng@gmail.com>
2022-08-04 10:58:26 +0900

    [S07P12D103-86] feat : 관심 운동 종목 컴포넌트로 분리

--- frontend/src/modules/init.js
+++ frontend/src/modules/init.js
@@ -1,3 +1,5 @@
+import { interestList } from "components/common/interestList";
+
 /* 액션 타입 */
 const SET_IMG = "init/SET_IMG";
 const SET_DESC = "init/SEC_DESC";
@@ -16,24 +18,7 @@ export const setInitFlag = (flag) => ({ type: SET_INIT_FLAG, flag });
 const initialState = {
   img: "",
   desc: "",
-  interest: [
-    { id: 1, value: "헬스", isInterested: false },
-    { id: 2, value: "요가", isInterested: false },
-    { id: 3, value: "필라테스", isInterested: false },
-    { id: 4, value: "러닝", isInterested: false },
-    { id: 5, value: "홈트레이닝", isInterested: false },
-    { id: 6, value: "축구", isInterested: false },
-    { id: 7, value: "야구", isInterested: false },
-    { id: 8, value: "농구", isInterested: false },
-    { id: 9, value: "테니스", isInterested: false },
-    { id: 10, value: "배드민턴", isInterested: false },
-    { id: 11, value: "등산", isInterested: false },
-    { id: 12, value: "수영", isInterested: false },
-    { id: 13, value: "골프", isInterested: false },
-    { id: 14, value: "볼링", isInterested: false },
-    { id: 15, value: "자전거/사이클", isInterested: false },
-    { id: 16, value: "기타", isInterested: false },
-  ],
+  interest: interestList,
   interestCnt: 0,
   initFlag: 0,
 };
