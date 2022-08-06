8d7b656f4841e4886f3c5271799fc4b059ddd10f
nullyng <nullyng@gmail.com>
2022-08-07 01:45:00 +0900

    [S07P12D103-110] chore : Navbar 스타일 수정
    .bold 클래스에 font-weight 적용하는 코드 다시 추가

--- frontend/src/components/common/TitleBar.js
+++ frontend/src/components/common/TitleBar.js
@@ -3,6 +3,10 @@ import { Box } from "@mui/system";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 function TitleBar(props) {
+  const handleClickTitle = () => {
+    document.querySelector(".content").scrollTo(0, 0);
+  };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar id="titlebar" position="static">
@@ -20,6 +24,7 @@ function TitleBar(props) {
             variant="h6"
             component="div"
             sx={{ flexGrow: 1 }}
+            onClick={handleClickTitle}
           >
             {props.title}
           </Typography>
