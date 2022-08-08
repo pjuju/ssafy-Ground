4ea953b0e4623e837236bfbbeaeee0b3990c8a40
qotlgus123 <qotlgus123@naver.com>
2022-08-08 14:23:34 +0900

    [S07P12D103-182] 팔로우, 게시글 알림 구현

--- backend/ground/src/main/java/com/ground/domain/user/dto/UserUpdateDto.java
+++ backend/ground/src/main/java/com/ground/domain/user/dto/UserUpdateDto.java
@@ -5,6 +5,8 @@ import com.ground.domain.user.entity.Age;
 import com.ground.domain.user.entity.Gender;
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 
 @NoArgsConstructor
 @Getter
@@ -18,12 +20,12 @@ public class UserUpdateDto {
     private String introduce;
 
     @Builder
-    public UserUpdateDto(String nickname, boolean privateYN, Age age, Gender gender, String introduce) {
-        this.nickname = nickname;
-        this.privateYN = privateYN;
-        this.age = age;
-        this.gender = gender;
-        this.introduce = introduce;
+    public UserUpdateDto(UserUpdateDto entity) {
+        this.nickname = entity.getNickname();
+        this.privateYN = entity.isPrivateYN();
+        this.age = entity.getAge();
+        this.gender = entity.getGender();
+        this.introduce = entity.getIntroduce();
     }
 }
 
