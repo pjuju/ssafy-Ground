81c01bb3ee8caa5ce0bf1fe4e21afa0862e72d4f
nullyng <nullyng@gmail.com>
2022-08-09 02:31:26 +0900

    [S07P12D103-89] feat : 모바일 반응형 AppBar 세분화
    최신 글 피드인 경우 타이틀 오른쪽에 필터 설정 버튼 출력

--- frontend/src/components/common/TitleBar.js
+++ frontend/src/components/common/TitleBar.js
@@ -1,6 +1,7 @@
 import { AppBar, Toolbar, IconButton, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import AutoAwesomeOutlinedIcon from "@mui/icons-material/AutoAwesomeOutlined";
 
 function TitleBar(props) {
   const handleClickTitle = () => {
@@ -8,29 +9,45 @@ function TitleBar(props) {
   };
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <Box sx={{ flexGrow: 1 }} display="flex" justifyContent="center">
       <AppBar id="titlebar" position="static">
         <Toolbar id="titlebar__toolbar">
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-          >
-            <ArrowBackIcon />
-          </IconButton>
+          {props.isBack ?
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+            >
+              <ArrowBackIcon />
+            </IconButton>
+            : (props.title === "최신 글 피드"
+              &&
+              <div style={{ width: '50.25px' }}>
+              </div>
+            )
+          }
           <Typography
             className="titlebar__text"
             variant="h6"
             component="div"
             sx={{ flexGrow: 1 }}
-            onClick={handleClickTitle}
           >
-            {props.title}
+            <span onClick={() => handleClickTitle()}>{props.title}</span>
           </Typography>
+          {props.title === "최신 글 피드" &&
+            <IconButton
+              size="large"
+              edge="center"
+              color="inherit"
+              aria-label="filter"
+            >
+              <AutoAwesomeOutlinedIcon />
+            </IconButton>
+          }
         </Toolbar>
       </AppBar>
-    </Box>
+    </Box >
   );
 }
 
