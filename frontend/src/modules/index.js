16122df43d81dbdf89dce1aacc05da22e856f805
Inhu Jo <inhudev@gmail.com>
2022-08-09 22:53:26 +0900

    chore : search reducer 삭제

--- frontend/src/modules/index.js
+++ frontend/src/modules/index.js
@@ -1,12 +1,10 @@
 import { combineReducers } from "redux";
 import init from "modules/init";
 import menu from "modules/menu";
-import search from "modules/search";
 
 const rootReducer = combineReducers({
   init,
   menu,
-  search
 });
 
 export default rootReducer;
