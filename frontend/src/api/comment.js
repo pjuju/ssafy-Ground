282eefe1cd1a9f70d53a5e74b51c1022711d582f
조인후 <lovelinus57@naver.com>
2022-08-12 03:09:07 +0900

    [S07P12D103-188] feat : 댓글 등록 구현

--- frontend/src/api/comment.js
+++ frontend/src/api/comment.js
@@ -3,15 +3,15 @@ import { apiInstance } from "api";
 const api = apiInstance();
 
 function registerComment(boardId, comment, success, fail) {
-  api.post(`/board/${boardId}/comment`, success, fail);
+  api.post(`/board/${boardId}/comment`, comment).then(success).catch(fail);
 }
 
 function updateComment(commentId, success, fail) {
-  api.put(`/comment/${commentId}`, success, fail);
+  api.put(`/comment/${commentId}`).then(success).catch(fail);
 }
 
 function deleteComment(commentId, success, fail) {
-  api.delete(`/comment/${commentId}`, success, fail);
+  api.delete(`/comment/${commentId}`).then(success).catch(fail);
 }
 
 export { registerComment, updateComment, deleteComment };
