e80abae0b0bf160c794833abf4e9b72cb1472cf5
Inhu Jo <inhudev@gmail.com>
2022-08-11 17:19:02 +0900

    chore : api 요청 주소 변경
    - 3000번에서 80번으로 변경

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 function apiInstance() {
   const instance = axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: "http://localhost:80/api",
     headers: {
       "Content-type": "application/json",
       ftoken: localStorage.getItem("token"),
