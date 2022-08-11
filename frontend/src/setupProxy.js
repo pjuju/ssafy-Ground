b0a3b19e07c13e3960ed9896173c5da2c9d7960b
Inhu Jo <inhudev@gmail.com>
2022-08-11 14:06:42 +0900

    chore : 프록시 설정 수정
    - setUpProxy의 target을 배포된 서버 주소로 수정

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     createProxyMiddleware("/api", {
-      target: "http://localhost:8080/rest",
+      target: "http://i7d103.p.ssafy.io:8080/rest",
       changeOrigin: true,
       pathRewrite: {
         "^/api": "/",
