75795e0520f263f6f95be91de0365474c24f3afe
Inhu Jo <inhudev@gmail.com>
2022-08-12 02:54:05 +0900

    [S07P12D103-188] feat : 댓글 등록 구현

--- frontend/src/components/Feed/Detail/ArticleDetailComment.js
+++ frontend/src/components/Feed/Detail/ArticleDetailComment.js
@@ -3,21 +3,34 @@ import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
 import Comment from "./Comment";
 import { useEffect, useState } from "react";
 import { getUserState } from "api/user";
-import { deleteComment } from "api/comment";
+import { deleteComment, registerComment } from "api/comment";
 import { useParams } from "react-router-dom";
+import CommentBox from "./CommentBox";
 
-function ArticleDetailComment({ comments }) {
+function ArticleDetailComment({ commentList }) {
   const { boardId } = useParams();
   const [userId, setUserId] = useState(0);
+  const [comments, setComments] = useState(commentList);
+
+  // 댓글 등록 핸들러
+  const handleCommentRegister = (comment) => {
+    registerComment(boardId, comment, (res) => {
+      const newComment = res.data;
+      setComments([...comments, newComment]);
+    });
+  };
 
   // 댓글 삭제 핸들러
   const handleCommentDelete = (commentId) => {
     if (window.confirm("댓글을 삭제하시겠습니까?")) {
-      deleteComment(commentId);
-      window.location.replace(`/feed/detail/${boardId}`);
+      deleteComment(commentId, (res) => {
+        const deletedComments = comments.filter((comment) => comment.id !== commentId);
+        setComments(deletedComments);
+      });
     }
   };
 
+  // 댓글 수정 핸들러
   const handleCommentEdit = (commentId) => {
     console.log("edit");
   };
@@ -33,7 +46,10 @@ function ArticleDetailComment({ comments }) {
       <IconButton>
         <ChatBubbleOutlineIcon />
       </IconButton>
-      댓글 <span className="bold">{comments.length}개</span>
+      <Grid item>
+        댓글 <span className="bold">{comments.length}개</span>
+      </Grid>
+      <CommentBox handleCommentRegister={handleCommentRegister} />
       {comments.map((comment, index) => (
         <Comment
           key={index}
