cd116a42f10921357079106d9a68ed4e70a06db5
Inhu Jo <inhudev@gmail.com>
2022-08-12 16:26:49 +0900

    [S07P12D103-117] feat : 최근 운동 뱃지 이미지 추가

--- frontend/src/components/Profile/UserExerciseRecord.js
+++ frontend/src/components/Profile/UserExerciseRecord.js
@@ -1,6 +1,3 @@
-import UserGroundBadge from "./static/UserGroundBadge";
-import UserGroundCalendar from "./static/UserGroundCalendar";
-import UserGroundGraph from "./static/UserGroundGraph";
 import UserGround from "./UserGround";
 
 const { Grid } = require("@mui/material");
@@ -10,14 +7,11 @@ function UserExerciseRecord() {
     <Grid className="user-ground">
       <Grid className="ground__title">
         <h2>나의 운동장</h2>
-        <UserGroundCalendar />
       </Grid>
       <Grid className="ground__content">
         <UserGround />
         <span className="ground__content__more">펼치기</span>
         <Grid display="none">더 많은 정보</Grid>
-        <UserGroundBadge />
-        <UserGroundGraph />
       </Grid>
     </Grid>
   );
