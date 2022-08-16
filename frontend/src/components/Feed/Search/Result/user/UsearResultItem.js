adbd86056a53a3b19c4e89112531a87b81ec0149
한유빈 <hyb120934@gmail.com>
2022-08-16 11:21:20 +0900

    [S07P12D103-45] front-feature/feed/search feat : 유저 프로필 사진 불러오기 구현

--- frontend/src/components/Feed/Search/Result/user/UsearResultItem.js
+++ frontend/src/components/Feed/Search/Result/user/UsearResultItem.js
@@ -1,13 +1,45 @@
 import { Grid } from "@mui/material";
-
+import { useEffect, useState } from "react";
 import userImage from "assets/images/userImage.png";
 import { useNavigate } from "react-router-dom";
+import { getDownloadURL, ref } from "firebase/storage";
+import { storage } from "api/firebase";
 
 function UserResultItem({ user }) {
+  const image = user.userImage
   const navigate = useNavigate();
+  const [profileImg, setProfileImg] = useState("");
+
+  useEffect(() => {
+    preview();
+    console.log(image)
+  });
+
+  useEffect(() => {
+    fetchImage();
+  }, [image]);
 
   const handleClickUser = () => {
     navigate(`/profile/${user.id}`);
+  };
+
+  const preview = () => {
+    if (profileImg === "") return false;
+    const imgElement = document.querySelector(".user-result__profile-wrapper > img");
+    if (imgElement !== null) {
+      imgElement.src = profileImg;
+    }
+  };
+
+  const fetchImage = () => {
+    const storageRef = ref(storage, `images/${image}`);
+
+    if (image !== undefined && image !== "") {
+      getDownloadURL(storageRef).then((url) => {
+        console.log("download");
+        setProfileImg(url);
+      });
+    }
   };
 
   return (
