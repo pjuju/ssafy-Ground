dbe26e0485c58b30084bfa96557223e3d5f2d526
InhuJo <inhudev@gmail.com>
2022-08-17 14:59:52 +0900

    fix : 게시글 작성 시간 수정

--- frontend/src/components/Feed/Article/ArticleInfo.js
+++ frontend/src/components/Feed/Article/ArticleInfo.js
@@ -16,45 +16,44 @@ function ArticleInfo(props) {
       date[4],
       date[5]
     );
-    let diff = new Date() - converted; // 차이(ms)
+    let diff = new Date() - converted + 32400000; // 차이(ms)
     const momentDate = date.filter((time, index) => index < 5);
     momentDate[1] = momentDate[1] - 1;
 
     // 차이가 1초 미만이라면
-    // if (diff < 1000) {
-    //   return "방금";
-    // }
+    if (diff < 1000) {
+      return "방금";
+    }
 
-    // let sec = Math.floor(diff / 1000); // 차이를 초로 변환
+    let sec = Math.floor(diff / 1000); // 차이를 초로 변환
 
-    // if (sec < 60) {
-    //   return sec + "초 전";
-    // }
+    if (sec < 60) {
+      return sec + "초 전";
+    }
 
-    // let min = Math.floor(diff / 60000); // 차이를 분으로 변환
-    // if (min < 60) {
-    //   return min + "분 전";
-    // }
+    let min = Math.floor(diff / 60000); // 차이를 분으로 변환
+    if (min < 60) {
+      return min + "분 전";
+    }
 
-    // let hour = Math.floor(min / 60); // 분을 시간으로 변환
-    // if (hour < 24) {
-    //   return hour + "시간 전";
-    // }
+    let hour = Math.floor(min / 60); // 분을 시간으로 변환
+    if (hour < 24) {
+      return hour + "시간 전";
+    }
 
-    // // 날짜의 포맷을 변경
-    // // 일, 월, 시, 분이 숫자 하나로 구성되어있는 경우, 앞에 0을 추가해줌
-    // let d = converted;
-    // d = [
-    //   "" + d.getFullYear(),
-    //   "0" + (d.getMonth() + 1),
-    //   "0" + d.getDate(),
-    //   "0" + d.getHours(),
-    //   "0" + d.getMinutes(),
-    // ].map((component) => component.slice(-2)); // 모든 컴포넌트의 마지막 숫자 2개를 가져옴
+    // 날짜의 포맷을 변경
+    // 일, 월, 시, 분이 숫자 하나로 구성되어있는 경우, 앞에 0을 추가해줌
+    let d = converted;
+    d = [
+      "" + d.getFullYear(),
+      "0" + (d.getMonth() + 1),
+      "0" + d.getDate(),
+      "0" + d.getHours(),
+      "0" + d.getMinutes(),
+    ].map((component) => component.slice(-2)); // 모든 컴포넌트의 마지막 숫자 2개를 가져옴
 
-    // // 컴포넌트를 조합
-    // return d.slice(0, 3).join(".") + " " + d.slice(3).join(":");
-    return moment(momentDate).fromNow();
+    // 컴포넌트를 조합
+    return d.slice(0, 3).join(".") + " " + d.slice(3).join(":");
   };
 
   const handleClickNickname = () => {
