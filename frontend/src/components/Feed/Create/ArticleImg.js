a9b5725c3c377bb3415c98c2dcf72e7b18e4ac06
InhuJo <inhudev@gmail.com>
2022-08-17 10:02:06 +0900

    merge from front to deploy

--- frontend/src/components/Feed/Create/ArticleImg.js
+++ frontend/src/components/Feed/Create/ArticleImg.js
@@ -1,6 +1,6 @@
 import { Grid } from "@mui/material";
 import { useEffect, useRef, useState } from "react";
-import plus from "assets/images/plus.png"
+import add from "assets/images/add.png"
 
 
 function ArticleImg({ boardInfo, newImages, setNewImages, uploadImages, setUploadImages }) {
@@ -14,12 +14,12 @@ function ArticleImg({ boardInfo, newImages, setNewImages, uploadImages, setUploa
     console.log("download complete")
     console.log(imgList)
     if (isDisplay === true) {
-      selectUserImg.current.value="";
+      selectUserImg.current.value = "";
     }
   }, [imgList]);
-  
 
-  
+
+
 
 
   const handleClickInput = (event) => {
@@ -29,8 +29,8 @@ function ArticleImg({ boardInfo, newImages, setNewImages, uploadImages, setUploa
     const fileName = file.name;
     const fileLength = fileName.length;
     const lastDot = fileName.lastIndexOf('.');
-    const fileSpec = fileName.substring(lastDot+1, fileLength).toLowerCase();
-    const randNum = parseInt((new Date().getTime() + Math.random())*100);
+    const fileSpec = fileName.substring(lastDot + 1, fileLength).toLowerCase();
+    const randNum = parseInt((new Date().getTime() + Math.random()) * 100);
     let imgUrlList = [...imgList];
     let fileUrlList = [...fileList];
     let imgNumList = [...newImages];
@@ -43,10 +43,10 @@ function ArticleImg({ boardInfo, newImages, setNewImages, uploadImages, setUploa
 
     if (event.target.files.length !== 0) {
       if (imgType.indexOf(fileSpec) !== -1) {
-        imgUrlList.push(["img",URL.createObjectURL(file)]);
+        imgUrlList.push(["img", URL.createObjectURL(file)]);
       }
       if (fileSpec === "mp4") {
-        imgUrlList.push(["video",URL.createObjectURL(file)]);
+        imgUrlList.push(["video", URL.createObjectURL(file)]);
       }
       imgNumList.push({
         imageType: fileSpec,
@@ -94,35 +94,35 @@ function ArticleImg({ boardInfo, newImages, setNewImages, uploadImages, setUploa
                 className="create-feed__img-button"
               >
                 <img
-                  src={plus}
-                  style={{ width: "100%", height: "100%" }}
+                  src={add}
+                  style={{ width: "100%", border: "1px solid #d3d3d3", borderRadius: "10px" }}
                   alt=""
                 />
               </button>
             </Grid>
           )}
-            {imgList.map((src, index) => (
-                <Grid item key={index} className="create-feed__media-wrapper">
-                  {src[0] === "img" && (
-                      <img
-                        src={src[1]}
-                        className="create-feed__media"
-                        alt=""
-                      />
-                  )}
+          {imgList.map((src, index) => (
+            <Grid item key={index} className="create-feed__media-wrapper">
+              {src[0] === "img" && (
+                <img
+                  src={src[1]}
+                  className="create-feed__media"
+                  alt=""
+                />
+              )}
 
-                  {src[0] === "video" && (       
-                    <video
-                      src={src[1]}
-                      className="create-feed__media"
-                      alt=""
-                      autoPlay
-                      controls
-                    />
-                  )}
-                  <button className="create-feed__delete-button" onClick={() => handleDeleteImage(index)}>x</button>
-                </Grid>
-            ))}
+              {src[0] === "video" && (
+                <video
+                  src={src[1]}
+                  className="create-feed__media"
+                  alt=""
+                  autoPlay
+                  controls
+                />
+              )}
+              <button className="create-feed__delete-button" onClick={() => handleDeleteImage(index)}>x</button>
+            </Grid>
+          ))}
         </Grid>
       </Grid>
     </Grid>
